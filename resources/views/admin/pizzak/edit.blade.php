c91662cc9503a7db84f113a907a5da1758901b63
subbee <subbee@subbee.com>
2025-11-13 18:11:33 +0100

    category select menu in pizza edit

--- resources/views/admin/pizzak/edit.blade.php
+++ resources/views/admin/pizzak/edit.blade.php
@@ -35,10 +35,19 @@
             {{-- Kategória neve --}}
             <div class="mb-4">
                 <label for="kategorianev" class="block text-sm font-medium text-gray-700">Kategória</label>
-                <input type="text" id="kategorianev" name="kategorianev"
-                       value="{{ old('kategorianev', $pizzak->kategorianev) }}"
-                       class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-green-500 focus:border-green-500"
-                       placeholder="Pl. apród, lovag, király...">
+
+                <select id="kategorianev" name="kategorianev"
+                        class="">
+                    @foreach(App\Models\Kategoria::all() as $kategoria)
+                        <option value="{{ $kategoria->nev }}"
+                            {{  old('kategorianev', $pizzak->kategorianev) == $kategoria->nev ? 'selected' : '' }}>
+                            {{ $kategoria->nev }}
+                        </option>
+                    @endforeach
+                </select>
+
+
+
             </div>
 
             {{-- Vegetáriánus --}}
